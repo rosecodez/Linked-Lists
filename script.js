9e0e6b71a9873f9639d14b59366c224cad04537d
Rose <rosebeats09@gmail.com>
2023-11-30 18:37:53 -0500

    Add size function to return the number of nodes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // node that contains 2 properties: its value and link to the nex one
 // both properties are null by default
-function Node(value = null, nextNode = null) {
+function Node(value, nextNode = null) {
   return { value, nextNode };
 }
 
@@ -10,53 +10,48 @@ class LinkedList {
     this.head = null;
   }
 
+  // function that adds a new node to the start of the list
+  prepend(value) {
+    // create new node object
+    const newNode = new Node(value);
+    // set pointer at the same node
+    newNode.nextNode = this.head;
+    this.head = newNode;
+    return this.head;
+  }
+
   // function that adds a new node to the end of the list
   append(value) {
     // create new node object
     const newNode = new Node(value);
     // if list is empty,
-    if (this.head === null) {
-      // set the head to the the node
-      this.head = newNode;
-    } else {
-      // start at the beginning of the list
-      let currentTail = this.head;
-      // traverse to the end of the list
-      while (currentTail.next !== null) {
-        currentTail = currentTail.next;
-      }
-      // point the current tail to the new node
-      currentTail.next = newNode;
-    }
-  }
-
-  // function that adds a new node to the start of the list
-  prepend(value) {
-    // create new node object
-    const newNode = new Node(value);
-    // if list is empty,
-    if (this.head === null) {
+    if (!this.head) {
       // set the head of the linkedList to the new node
       this.head = newNode;
-    } else {
-      // find the current head node
-      const currentHead = this.head;
-      // reassign the head to our new node
-      this.head = newNode;
-      // set its next pointer to the current head
-      newNode.next = currentHead;
+      return this.head;
     }
+    let tail = this.head;
+    while (tail.nextNode !== null) {
+      tail = tail.nextNode;
+    }
+    tail.nextNode = newNode;
+    return this.head;
+  }
+
+  // function that return the total number of nodes in the list
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.nextNode;
+    }
+    console.log(`count:${count}`);
+    return count;
   }
 }
-// create 2 nodes
-const node1 = new Node(1);
-const node2 = new Node(2);
-
-// assign link between nodes
-node1.nextNode = node2;
-
-// create linked list
-const list = new LinkedList(node1);
-
-// access nodes
-console.log(list.list.nextNode.value);
+const list = new LinkedList();
+list.append(5);
+list.prepend(2);
+console.log(list);
+list.size();
